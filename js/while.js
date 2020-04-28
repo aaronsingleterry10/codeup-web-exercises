a4f9ca1d079a5a924b00d7c2a3cc7abaea4de287
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-28 15:38:09 -0500

    completed while.js exercise

--- js/while.js
+++ js/while.js
@@ -8,4 +8,25 @@
         console.log(i);
         i = i * 2;
     }
+
+    var allCones = Math.floor(Math.random() * 50) + 50;
+
+    console.log(allCones);
+
+    do {
+        var conesBought = Math.floor(Math.random() * 5) + 1;
+
+        if (allCones >= conesBought) {
+            console.log(conesBought + ' cones sold');
+        } else {
+            console.log('Cannot sell you ' + conesBought + ' cones. I only have ' + allCones + '...');
+        }
+
+        if (allCones === 0) {
+            console.log('Yay! I sold them all!');
+        }
+
+        allCones = allCones - conesBought;
+    } while (allCones >= 0)
+
 })();
