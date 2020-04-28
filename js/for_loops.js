13894e6d8c9f5bea933823997646969b50f1d17c
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-28 13:43:13 -0500

    completed for_loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(function () {
+
+    function showMultiplicationTable(num) {
+        for (var i = 1; i <= 10; i++) {
+            console.log(num + ' x ' + i + ' = ' + (num * i));
+        }
+    }
+
+    showMultiplicationTable(7)
+})();
