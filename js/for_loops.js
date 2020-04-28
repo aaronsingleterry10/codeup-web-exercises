26b9b556b9d6b6996f348b090dabd776c3c29419
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-28 14:17:23 -0500

    completed exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,5 +8,17 @@
         }
     }
 
-    showMultiplicationTable(7)
+    showMultiplicationTable(7);
+
+    for (var i = 1; i <= 10; i++) {
+
+        var randomNumber = Math.floor((Math.random() * 200) + 20);
+
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + ' is even');
+        } else {
+            console.log(randomNumber + ' is odd');
+        }
+    }
+
 })();
