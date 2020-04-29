0e5d5cc6c89bf7ac1e6227ae427a1879eaf63435
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-29 10:02:27 -0500

    completed execise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,23 +4,32 @@
 
     while (true) {
         var numberInput = prompt('Enter a number between 1 and 50');
+
         if (numberInput <= 50 && numberInput >= 1 && !isNaN(numberInput)) {
+            oddNumber();
+            break;
+        } else if (numberInput === '') {
+            alert('That is an invalid number');
+        } else if (numberInput === null) {
             break;
         } else {
             alert('That is an invalid number');
         }
     }
 
-    console.log('Number to skip is: ' + numberInput);
+    function oddNumber() {
 
-    for (var i = 1; i <= 50; i++) {
-        if (i % 2 === 0){
-            continue;
-        }
-        if (parseFloat(numberInput) === i) {
-             console.log('Yikes! Skipping number: ' + i);
-        } else {
-            console.log('Here is an odd number: ' + i);
+        console.log('Number to skip is: ' + numberInput);
+
+        for (var i = 1; i <= 50; i++) {
+            if (i % 2 === 0) {
+                continue;
+            }
+            if (parseFloat(numberInput) === i) {
+                console.log('Yikes! Skipping number: ' + i);
+            } else {
+                console.log('Here is an odd number: ' + i);
+            }
         }
     }
 
