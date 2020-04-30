05a04d0bd11d6adedbb108d1b81e1e4f9a9f5ef4
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-29 22:10:04 -0500

    completed refactoring code

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,38 +2,38 @@
 
 (function () {
 
-    // function showMultiplicationTable(num) {
-    //     var output = ''
-    //     for (var i = 1; i <= 10; i++) {
-    //         output = output + num + ' x ' + i + ' = ' + (num * i) + '\n';
-    //     }
-    //     return output;
-    // }
-    //
-    // console.log(showMultiplicationTable(7));
+    function showMultiplicationTable(num) {
+        var output = ''
+        for (var i = 1; i <= 10; i++) {
+            output = output + num + ' x ' + i + ' = ' + (num * i) + '\n';
+        }
+        return output;
+    }
 
-    // function isOddOrEven(num) {
-    //     return (num % 2 === 0) ? num + ' is even.' : num + ' is odd';
-    // }
-    //
-    // function randomNumberGenerator(min, max) {
-    //     return Math.floor(Math.random() * (max - min)) + min;;
-    // }
-    //
-    // function isRandomNumberOddOrEven(num) {
-    //     var output = '';
-    //     for (var i = 1; i <= num; i++) {
-    //       output += isOddOrEven(randomNumberGenerator(20, 200)) + '\n';
-    //     }
-    //     return output;
-    // }
-    //
-    // console.log(isRandomNumberOddOrEven(10));
+    console.log(showMultiplicationTable(7));
+
+    function isOddOrEven(num) {
+        return (num % 2 === 0) ? num + ' is even.' : num + ' is odd';
+    }
+
+    function randomNumberGenerator(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;;
+    }
+
+    function isRandomNumberOddOrEven(num) {
+        var output = '';
+        for (var i = 1; i <= num; i++) {
+          output += isOddOrEven(randomNumberGenerator(20, 200)) + '\n';
+        }
+        return output;
+    }
+
+    console.log(isRandomNumberOddOrEven(10));
 
     for (var x = 1; x <= 9; x++) {
          console.log(x.toString().repeat(x));
     }
-
+    // refactored code
     for (var i = 1; i <= 9; i++) {
         var output = '';
         for (var j = 1; j <= i; j++) {
@@ -42,10 +42,13 @@
         console.log(output);
     }
 
-    //
-    //
-    // for (var num = 100; num >= 5; num = num - 5) {
-    //     console.log(num);
-    // }
+    function countingDown(from, to, takeAway) {
+        var output = '';
+        for (var num = from; num >= to; num = num - takeAway) {
+            output += num + '\n';
+        }
+        return output;
+    }
 
+    console.log(countingDown(100, 5, 5));;
 })();
