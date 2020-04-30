efe3c0687ccbb2abdc9e0c997e246841e93d338e
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-29 22:23:57 -0500

    refactoring code on break and continue, not finished

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,35 +2,48 @@
 
 (function () {
 
-    while (true) {
-        var numberInput = prompt('Enter an odd number between 1 and 50');
+    // while (true) {
+    //     var numberInput = prompt('Enter an odd number between 1 and 50');
+    //
+    //     if (numberInput <= 50 && numberInput >= 1 && numberInput % 2 !== 0 && !isNaN(numberInput)) {
+    //         oddNumber();
+    //         break;
+    //     } else if (numberInput === '') {
+    //         alert('That is an invalid number');
+    //     } else if (numberInput === null) {
+    //         break;
+    //     } else {
+    //         alert('That is an invalid number');
+    //     }
+    // }
+    //
+    // function oddNumber() {
+    //
+    //     console.log('Number to skip is: ' + numberInput);
+    //
+    //     for (var i = 1; i <= 50; i++) {
+    //         if (i % 2 === 0) {
+    //             continue;
+    //         }
+    //         if (parseFloat(numberInput) === i) {
+    //             console.log('Yikes! Skipping number: ' + i);
+    //         } else {
+    //             console.log('Here is an odd number: ' + i);
+    //         }
+    //     }
+    // }
 
-        if (numberInput <= 50 && numberInput >= 1 && numberInput % 2 !== 0 && !isNaN(numberInput)) {
-            oddNumber();
-            break;
-        } else if (numberInput === '') {
-            alert('That is an invalid number');
-        } else if (numberInput === null) {
-            break;
-        } else {
-            alert('That is an invalid number');
-        }
+    function isNumberValid(num) {
+        return num <= 50 && num >= 1 && num % 2 !== 0 && !isNaN(num);
     }
 
-    function oddNumber() {
+    // function toSkipOrNot(num) {
+    //     if (num)
+    // }
 
-        console.log('Number to skip is: ' + numberInput);
-
-        for (var i = 1; i <= 50; i++) {
-            if (i % 2 === 0) {
-                continue;
-            }
-            if (parseFloat(numberInput) === i) {
-                console.log('Yikes! Skipping number: ' + i);
-            } else {
-                console.log('Here is an odd number: ' + i);
-            }
-        }
+    var inputPrompt = function () {
+        return prompt('Enter an odd number 1 - 50.');
     }
+
 
 })();
