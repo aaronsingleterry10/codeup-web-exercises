7ddb47abf001657b68a6bbd24e5796e66f52d6b7
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-04-29 20:42:56 -0500

    tweaked the function on line 5

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,31 +3,33 @@
 (function () {
 
     function showMultiplicationTable(num) {
+        var output = ''
         for (var i = 1; i <= 10; i++) {
-            console.log(num + ' x ' + i + ' = ' + (num * i));
+            output = output + num + ' x ' + i + ' = ' + (num * i) + '\n';
         }
+        return output;
     }
 
-    showMultiplicationTable(7);
+    console.log(showMultiplicationTable(7));
 
-    for (var i = 1; i <= 10; i++) {
-
-        var randomNumber = Math.floor((Math.random() * 200 - 20 + 1) + 20);
-
-        if (randomNumber % 2 === 0) {
-            console.log(randomNumber + ' is even');
-        } else {
-            console.log(randomNumber + ' is odd');
-        }
-    }
-
-    for (var x = 1; x <= 9; x++) {
-         console.log(x.toString().repeat(x));
-    }
-
-
-    for (var num = 100; num >= 5; num = num - 5) {
-        console.log(num);
-    }
+    // for (var i = 1; i <= 10; i++) {
+    //
+    //     var randomNumber = Math.floor((Math.random() * 200 - 20 + 1) + 20);
+    //
+    //     if (randomNumber % 2 === 0) {
+    //         console.log(randomNumber + ' is even');
+    //     } else {
+    //         console.log(randomNumber + ' is odd');
+    //     }
+    // }
+    //
+    // for (var x = 1; x <= 9; x++) {
+    //      console.log(x.toString().repeat(x));
+    // }
+    //
+    //
+    // for (var num = 100; num >= 5; num = num - 5) {
+    //     console.log(num);
+    // }
 
 })();
