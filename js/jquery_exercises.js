158877573e7be093ed6026754d11179e64a908db
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-05-13 11:16:09 -0500

    completed exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,9 +2,32 @@
 (function() {
     $(document).ready(function() {
 
-        $('li').css('font-size', '20px');
-        $('h1, p, li');
-        alert($('h1'));
+        // alert($('#main-paragraph').html());
+        // alert($('#main-heading').html());
+        // $('.codeup').css('border', '1px solid red');
+        // $('li').css({
+        //     'font-size': '20px',
+        //     'background-color': 'red'
+        // });
+        // $('h1').css('background-color', 'yellow');
+        // $('p').css('background-color', 'green');
+        // $('h1, p, li').css('background-color', 'yellow');
+
+        // === jQuery Mouse Events Exercise ===
+
+        $('h1').click(function() {
+            $(this).css("background-color", "green");
+        });
+        $('p').dblclick(function() {
+            $(this).css("font-size", "18px");
+        });
+
+        $('li').hover(function() {
+            $(this).css("color", "red");
+        }, function() {
+            $(this).css("color", "black");
+        });
+
 
     });
 })();
