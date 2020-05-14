e04e0d3f90e10fc84ad25f0e43e2c9ffead3b761
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-05-14 11:13:41 -0500

    completed first half

--- js/essential-methods-lec.js
+++ js/essential-methods-lec.js
@@ -30,16 +30,29 @@ $(document).ready(function() {
 
     // TODO TOGETHER:  Using an event listener and css method, increase the font-size of 'Sorting Hat' when clicked.
     //    How can font size be changed using .html()?
-
+        $('#main-heading').click(function() {
+            $(this).css('font-size', '4em');
+            // $(this).html('<h4>My Sorting Hat</h4>');
+        });
     // TODO TOGETHER: store the inner text of #main-heading in a variable
-
+        var mainHeading = $('#main-heading').text();
+        // alert(mainHeading);
     // TODO: Using a css method, change the banner background-color to black
+        $('.banner').css('background-color', 'black');
     //  Hint: make sure you use the right selector
 
     // TODO TOGETHER: When the 'Houses' button is clicked, add the class 'house' to anything with a class of 'house-name'
 
+        // $('#highlight-houses').click(function() {
+            // $('.house-name').addClass('house');
+        // });
+
     // TODO: Comment out the code above. Add the class of 'house' to all the 'house-names'
     //  Write the event listener to remove the class instead
+        $('#highlight-houses').click(function() {
+            // $('.house-name').removeClass('house');
+            $('.house-name').toggleClass('house');
+        })
 
     // TODO TOGETHER: Refactor your addClass and removeClass methods to toggle the 'house' class instead
 
