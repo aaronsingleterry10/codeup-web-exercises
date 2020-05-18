1ca25f6203bb22984e2ab805cc05d3dc12487e1d
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-05-18 10:16:49 -0500

    added another ajax request

--- js/joke-and-quote-page.js
+++ js/joke-and-quote-page.js
@@ -3,12 +3,24 @@
 
 $(document).ready(function() {
 
-    $.ajax('https://api.icndb.com/jokes/random').done(function(resp) {
-
-        $('#left-btn-joke').click(function() {
-            // console.log(resp.value.joke);
-           $('#joke-quote-display').html(resp.value.joke);
+    function joke() {
+        $.ajax('https://api.icndb.com/jokes/random').done(function (resp) {
+            $('#joke-quote-display').html(resp.value.joke);
         });
+    }
+
+    function quote() {
+        $.ajax('http://quotes.stormconsultancy.co.uk/random.json').done(function (resp) {
+            $('#joke-quote-display').html('"' + resp.quote + '"' + " -" + resp.author);
+        });
+    }
+
+    $('#left-btn-joke').click(function () {
+        joke();
+    });
+
+    $('#right-btn-quote').click(function () {
+        quote();
     });
 
 });
