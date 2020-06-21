558441f1edbbacaad9717f935c7d6bed4f801bcc
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-21 13:29:34 -0500

    completed mini project

--- js/change-bg-color.js
+++ js/change-bg-color.js
@@ -16,8 +16,11 @@
         "steelblue"
     ];
 
-    let randomNumber = Math.floor(Math.random() * ((colorsArray.length - 1) - 0 + 1));
-    console.log(randomNumber);
+    document.getElementById("change-bg-color").addEventListener("click", function(e) {
+        let randomNumber = Math.floor(Math.random() * ((colorsArray.length - 1) - 0 + 1));
 
+        document.getElementsByTagName("body")[0].style.backgroundColor = `${colorsArray[randomNumber]}`;
+        document.getElementById("change-bg-color").style.backgroundColor = `${colorsArray[randomNumber]}`;
+    });
 
 })();
