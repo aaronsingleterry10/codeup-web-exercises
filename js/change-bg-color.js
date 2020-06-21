e121c3b99a3d869d6e6ef011eca5e88694e87ed3
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-21 13:02:34 -0500

    added randomNumber generator

--- js/change-bg-color.js
+++ js/change-bg-color.js
@@ -1,7 +1,23 @@
 "use strict";
 
 (function() {
-    console.log("test");
+
+    let colorsArray = [
+        "lightcoral",
+        "hotpink",
+        "orange",
+        "yellow",
+        "fuchsia",
+        "purple",
+        "limegreen",
+        "darkgreen",
+        "teal",
+        "olive",
+        "steelblue"
+    ];
+
+    let randomNumber = Math.floor(Math.random() * ((colorsArray.length - 1) - 0 + 1));
+    console.log(randomNumber);
 
 
 })();
